e63c55d21577668e9284aafe2bf452d79376cfcb
Valentin <portais.valentin@gmail.com>
2018-01-16 16:31:34 +0100

    Inscription v1

--- Controller/user.php
+++ Controller/user.php
@@ -27,13 +27,7 @@ switch ($action){
         // Nous indiquons la vue à charger
         $view = "list";
         break;
-    case "connectForm":
-        if(is_null($userConnected)) {
-            $view = "connectForm";
-        }else{
-            header("Location: index.php");
-        }
-        break;
+
     case "connect":
         $errors = [];
 
@@ -60,7 +54,8 @@ switch ($action){
             }
         }
         else{
-            $errors[] = "Attention, il manque l'email ou le mot de passe";
+            $view = "connectForm";
+            break;
         }
 
 
@@ -71,12 +66,93 @@ switch ($action){
         }
         break;
 
+    case "signUpForm":
+        $errors = [];
+        if(is_null($userConnected)) $view = "connectForm";
+        else header("Location: index.php");
+        break;
+
+
+    case "signUp":
+        $errors = [];
+
+        //On vérifie une à une la validité des champs rentrés
+        if (isset($_POST["firstname"])) {
+            if(!preg_match("#^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð '-]+$#"), $_POST["fistname"])) {
+                $errors['firstname']='Veuillez entre un prénom valide';
+            }
+        }
+        else {
+                $errors['firstname']='Veuillez entre un prénom';
+        }   
+
+        if (isset($_POST["name"])) {
+            if(!preg_match("#^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð '-]+$#"), $_POST["name"])) {
+                $errors['name']='Veuillez entre un nom valide';
+            }
+        }
+        else {
+                $errors['name']='Veuillez entre un nom';
+        } 
+
+        if (isset($_POST["email"])) {
+            if (preg_match( "#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $_POST['mail']))
+            {
+                $searchUserByEmail = User::selectWhere(array("email" => $_POST["email"]));
+                $numberOfUserWith = count($searchUserByUserEmail);
+                if($numberOfUserWith > 0){
+                    $errors['email'] = "L'email est déjà utilisé !";
+                }
+            }
+            else {
+                $errors['email']='Veuillez entre un email !';
+            }
+        }
+
+        if (isset($_POST["password"])) {
+            if (!empty($_POST["password"]))
+            {
+                
+            }
+        }
+
+        if (isset($_POST["password_conf"])) {
+            if (!empty($_POST["password_conf"]))
+            {
+
+            }
+        }
+
+        if (isset($_POST["phonenumber"])) {
+            if (!empty($_POST["phonenumber"]))
+            {
+
+            }
+        }
+
+        if (isset($_POST["datebirth"]))
+             {
+            if (!empty($_POST["datebirth"]))
+            {
+
+            }
+        }
+
+        if (isset($_POST["city"])) {
+            if (!empty($_POST["city"]))
+            {
+
+            }
+        }
+
+
     case "disconnect":
         if(!is_null($userConnected)){
             session_destroy();
         }
         header("Location: index.php");
         break;
+
     case "":
 
         break;
--- View/template.php
+++ View/template.php
@@ -3,13 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <title>Magenda</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
 <body>
-<?php
-var_dump($_SESSION);
-?>
-<?php
-    require_once __DIR__ ."/".$controller."/".$view.".php";
-?>
+	<?php
+	var_dump($_SESSION);
+	?>
+	<?php
+	    require_once __DIR__ ."/".$controller."/".$view.".php";
+	?>
 </body>
+
+	<!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Javascript de Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
 </html>
